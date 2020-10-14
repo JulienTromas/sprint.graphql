75e4409888035953d49b733d4c041442ee75a393
Julien Tromas <julien.tromas@gmail.com>
2020-10-14 14:13:41 +0900

    Wrote the schema to return a Pokemon and it returns the right informations
    Co-authored-by: OnigiriJack <onigirijack@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- server/schema.js
+++ server/schema.js
@@ -7,7 +7,51 @@ const typeDefs = gql`
   type Pokemon {
     id: String
     name: String!
+    classification: String!
+    types: [String!]
+    resistant: [String!]
+    weaknesses: [String!]
+    weight: Weight!
+    height: Height!
+    fleeRate: Float
+    evolutionRequirements: EvolutionRequirements!
+    evolutions: [Evolution]
+    maxCP: Int
+    maxHP: Int
+    attacks: Attacks!
   }
+
+  type EvolutionRequirements {
+    amount: Int!
+    name: String!
+  }
+
+  type Weight {
+    minimum: String!
+    maximum: String!
+  }
+
+  type Height {
+    minimum: String!
+    maximum: String!
+  }
+
+  type Attacks {
+    fast: [Attack]
+    special: [Attack]
+  }
+
+  type Attack {
+    name: String!
+    type: String!
+    damage: Int!
+  }
+
+  type Evolution {
+    id: Int!
+    name: String!
+  }
+
   type Query {
     Pokemons: [Pokemon]
     Pokemon(name: String!): Pokemon
